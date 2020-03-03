45f77a86296a6bf46b50aac2b940d6f32c240027
MEYTA <43209941+serdaraltin@users.noreply.github.com>
2020-03-03 20:46:21 +0300

    Add files via upload

---
+++ Dork_Tarayıcı/Form1.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ınfluenceTheme1 = New Dork_Tarayıcı.InfluenceTheme()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ınfluenceButton3 = New Dork_Tarayıcı.InfluenceButton()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.ınfluenceButton2 = New Dork_Tarayıcı.InfluenceButton()
+        Me.ınfluenceComboBox1 = New Dork_Tarayıcı.InfluenceComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ınfluenceProgressBar1 = New Dork_Tarayıcı.InfluenceProgressBar()
+        Me.ınfluenceTextBox1 = New Dork_Tarayıcı.InfluenceTextBox()
+        Me.ınfluenceListBox1 = New Dork_Tarayıcı.InfluenceListBox()
+        Me.ınfluenceButton1 = New Dork_Tarayıcı.InfluenceButton()
+        Me.ınfluenceTheme1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(15, 89)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.Size = New System.Drawing.Size(300, 300)
+        Me.WebBrowser1.TabIndex = 3
+        Me.WebBrowser1.Visible = False
+        '
+        'ınfluenceTheme1
+        '
+        Me.ınfluenceTheme1.CloseButtonExitsApp = False
+        Me.ınfluenceTheme1.Controls.Add(Me.Label2)
+        Me.ınfluenceTheme1.Controls.Add(Me.ınfluenceButton3)
+        Me.ınfluenceTheme1.Controls.Add(Me.LinkLabel1)
+        Me.ınfluenceTheme1.Controls.Add(Me.ınfluenceButton2)
+        Me.ınfluenceTheme1.Controls.Add(Me.ınfluenceComboBox1)
+        Me.ınfluenceTheme1.Controls.Add(Me.Label1)
+        Me.ınfluenceTheme1.Controls.Add(Me.ınfluenceProgressBar1)
+        Me.ınfluenceTheme1.Controls.Add(Me.ınfluenceTextBox1)
+        Me.ınfluenceTheme1.Controls.Add(Me.ınfluenceListBox1)
+        Me.ınfluenceTheme1.Controls.Add(Me.ınfluenceButton1)
+        Me.ınfluenceTheme1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ınfluenceTheme1.Font = New System.Drawing.Font("Verdana", 8.25!)
+        Me.ınfluenceTheme1.Location = New System.Drawing.Point(0, 0)
+        Me.ınfluenceTheme1.MinimizeButton = False
+        Me.ınfluenceTheme1.Name = "ınfluenceTheme1"
+        Me.ınfluenceTheme1.Size = New System.Drawing.Size(326, 430)
+        Me.ınfluenceTheme1.TabIndex = 4
+        Me.ınfluenceTheme1.Text = "Dork Tarayıcı"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(170, 408)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(145, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Powered By DeadSound"
+        '
+        'ınfluenceButton3
+        '
+        Me.ınfluenceButton3.BackColor = System.Drawing.Color.Transparent
+        Me.ınfluenceButton3.Font = New System.Drawing.Font("Verdana", 8.25!)
+        Me.ınfluenceButton3.Location = New System.Drawing.Point(232, 0)
+        Me.ınfluenceButton3.Name = "ınfluenceButton3"
+        Me.ınfluenceButton3.Size = New System.Drawing.Size(39, 21)
+        Me.ınfluenceButton3.TabIndex = 10
+        Me.ınfluenceButton3.Text = "  _"
+        Me.ınfluenceButton3.TextAlignment = System.Drawing.StringAlignment.Near
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.Aqua
+        Me.LinkLabel1.Location = New System.Drawing.Point(12, 406)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(74, 13)
+        Me.LinkLabel1.TabIndex = 9
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Sorunu Çöz"
+        '
+        'ınfluenceButton2
+        '
+        Me.ınfluenceButton2.BackColor = System.Drawing.Color.Transparent
+        Me.ınfluenceButton2.Font = New System.Drawing.Font("Verdana", 8.25!)
+        Me.ınfluenceButton2.Location = New System.Drawing.Point(219, 68)
+        Me.ınfluenceButton2.Name = "ınfluenceButton2"
+        Me.ınfluenceButton2.Size = New System.Drawing.Size(95, 25)
+        Me.ınfluenceButton2.TabIndex = 8
+        Me.ınfluenceButton2.Text = "     Kaydet"
+        Me.ınfluenceButton2.TextAlignment = System.Drawing.StringAlignment.Near
+        '
+        'ınfluenceComboBox1
+        '
+        Me.ınfluenceComboBox1.BackColor = System.Drawing.Color.Transparent
+        Me.ınfluenceComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.ınfluenceComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ınfluenceComboBox1.ForeColor = System.Drawing.Color.White
+        Me.ınfluenceComboBox1.FormattingEnabled = True
+        Me.ınfluenceComboBox1.ItemHighlightColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ınfluenceComboBox1.Items.AddRange(New Object() {"10", "20", "30", "40", "50", "100"})
+        Me.ınfluenceComboBox1.Location = New System.Drawing.Point(92, 69)
+        Me.ınfluenceComboBox1.Name = "ınfluenceComboBox1"
+        Me.ınfluenceComboBox1.Size = New System.Drawing.Size(80, 22)
+        Me.ınfluenceComboBox1.StartIndex = 0
+        Me.ınfluenceComboBox1.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(12, 73)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Site Sayısı"
+        '
+        'ınfluenceProgressBar1
+        '
+        Me.ınfluenceProgressBar1.BackColor = System.Drawing.Color.Transparent
+        Me.ınfluenceProgressBar1.Location = New System.Drawing.Point(12, 375)
+        Me.ınfluenceProgressBar1.Maximum = 100
+        Me.ınfluenceProgressBar1.Name = "ınfluenceProgressBar1"
+        Me.ınfluenceProgressBar1.ShowPercentage = False
+        Me.ınfluenceProgressBar1.Size = New System.Drawing.Size(302, 23)
+        Me.ınfluenceProgressBar1.TabIndex = 5
+        Me.ınfluenceProgressBar1.Text = "ınfluenceProgressBar1"
+        Me.ınfluenceProgressBar1.Value = 0
+        '
+        'ınfluenceTextBox1
+        '
+        Me.ınfluenceTextBox1.BackColor = System.Drawing.Color.Transparent
+        Me.ınfluenceTextBox1.ForeColor = System.Drawing.Color.White
+        Me.ınfluenceTextBox1.Location = New System.Drawing.Point(12, 38)
+        Me.ınfluenceTextBox1.MaxLength = 32767
+        Me.ınfluenceTextBox1.Name = "ınfluenceTextBox1"
+        Me.ınfluenceTextBox1.Size = New System.Drawing.Size(201, 25)
+        Me.ınfluenceTextBox1.TabIndex = 4
+        Me.ınfluenceTextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.ınfluenceTextBox1.UseSystemPasswordChar = False
+        '
+        'ınfluenceListBox1
+        '
+        Me.ınfluenceListBox1.BackColor = System.Drawing.Color.Transparent
+        Me.ınfluenceListBox1.Items = New String() {""}
+        Me.ınfluenceListBox1.Location = New System.Drawing.Point(12, 96)
+        Me.ınfluenceListBox1.Name = "ınfluenceListBox1"
+        Me.ınfluenceListBox1.Size = New System.Drawing.Size(302, 275)
+        Me.ınfluenceListBox1.TabIndex = 3
+        Me.ınfluenceListBox1.Text = "ınfluenceListBox1"
+        '
+        'ınfluenceButton1
+        '
+        Me.ınfluenceButton1.BackColor = System.Drawing.Color.Transparent
+        Me.ınfluenceButton1.Font = New System.Drawing.Font("Verdana", 8.25!)
+        Me.ınfluenceButton1.Location = New System.Drawing.Point(219, 38)
+        Me.ınfluenceButton1.Name = "ınfluenceButton1"
+        Me.ınfluenceButton1.Size = New System.Drawing.Size(95, 25)
+        Me.ınfluenceButton1.TabIndex = 2
+        Me.ınfluenceButton1.Text = "     Tarama"
+        Me.ınfluenceButton1.TextAlignment = System.Drawing.StringAlignment.Near
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(326, 430)
+        Me.Controls.Add(Me.ınfluenceTheme1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Dork Tarayıcı"
+        Me.ınfluenceTheme1.ResumeLayout(False)
+        Me.ınfluenceTheme1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ınfluenceTheme1 As Dork_Tarayıcı.InfluenceTheme
+    Friend WithEvents ınfluenceButton1 As Dork_Tarayıcı.InfluenceButton
+    Friend WithEvents ınfluenceListBox1 As Dork_Tarayıcı.InfluenceListBox
+    Friend WithEvents ınfluenceTextBox1 As Dork_Tarayıcı.InfluenceTextBox
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents ınfluenceProgressBar1 As Dork_Tarayıcı.InfluenceProgressBar
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ınfluenceComboBox1 As Dork_Tarayıcı.InfluenceComboBox
+    Friend WithEvents ınfluenceButton2 As Dork_Tarayıcı.InfluenceButton
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents ınfluenceButton3 As Dork_Tarayıcı.InfluenceButton
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Dork_Tarayıcı/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports System.IO
+Public Class Form1
+    Dim count As Integer = 0
+    Dim metin As String = ""
+    Dim site As Integer = 0
+    Private Sub ınfluenceButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ınfluenceButton1.Click
+        WebBrowser1.Navigate("https://www.google.com.tr/#q=" + ınfluenceTextBox1.Text.ToString + "3D&ei=PQyHWcySDcmOgAb9so1Y&start=10")
+        ınfluenceListBox1.NewListBox()
+        metin = ""
+        site = Convert.ToInt32(ınfluenceComboBox1.Text.ToString)
+        ınfluenceProgressBar1.Value = 0
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(ByVal sender As System.Object, ByVal e As System.Windows.Forms.WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        Try
+            count = 0
+            'ınfluenceListBox1.NewListBox()
+            Dim bilgi As HtmlElementCollection = WebBrowser1.Document.All
+            For Each element As HtmlElement In bilgi
+                If (element.GetAttribute("classname").Contains("_Rm")) Then
+                    ınfluenceListBox1.AddItem(element.InnerText)
+                    metin += element.InnerText + Environment.NewLine
+                    count += 1
+                End If
+            Next
+
+            site -= 10
+            If (site <> 0) Then
+                WebBrowser1.Navigate("https://www.google.com.tr/#q=" + ınfluenceTextBox1.Text.ToString + "3D&ei=PQyHWcySDcmOgAb9so1Y&start=" + (Convert.ToInt32(ınfluenceComboBox1.Text.ToString) - site).ToString)
+                ınfluenceProgressBar1.Value = Convert.ToInt32(ınfluenceComboBox1.Text.ToString) - site
+            End If
+            If (site = 0) Then
+                ınfluenceProgressBar1.Value = 100
+                MessageBox.Show("Dork sonuçları listelendi", "Dork Tarayıcı", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString + Environment.NewLine + "Eğer bir sorunla karşılaşırsanız Sorun çöz seçeneğini deneyin", "Dork Tarayıcı", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub ınfluenceButton2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ınfluenceButton2.Click
+        If (metin <> "") Then
+            Dim kaydet As SaveFileDialog = New SaveFileDialog
+            kaydet.Title = "Kaydet"
+            kaydet.Filter = "Metin Dosyaları|*.txt|Tüm Dosyalar|*.*"
+            If (kaydet.ShowDialog() = DialogResult.OK) Then
+                System.IO.File.CreateText(kaydet.FileName).Close()
+                Dim yaz As System.IO.StreamWriter = New System.IO.StreamWriter(kaydet.FileName)
+                yaz.Write("Dork tarayıcı | Powered By DeadSound" + Environment.NewLine + Environment.NewLine + metin)
+                yaz.Close()
+                MessageBox.Show("Dosya oluşturldu.", "Dork Tarayıcı", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        End If
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        System.Diagnostics.Process.Start("https://www.google.com.tr/#q=" + "Book.php?bookID=" + "3D&ei=PQyHWcySDcmOgAb9so1Y&start=10")
+    End Sub
+
+    Private Sub ınfluenceButton3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ınfluenceButton3.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub Form1_Load_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        WebBrowser1.ScriptErrorsSuppressed = True
+    End Sub
+End Class
